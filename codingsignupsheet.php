baf9199a6262cdaf605116cc144d0fe3e2e66f2b
bludandvomit <kedokukuto.joou@gmail.com>
2024-11-06 15:03:46 +0300

    Add files via upload

---
+++ codingsignupsheet.php
@@ -0,0 +1,28 @@
+<form action="action_page.php" style="border:2px solid teal">
+  <div class="container">
+    <h1>999</h1>
+    <p>We need your sigma details, do it.</p>
+    <hr>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Sell thy Soul" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+
+    <label>
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+
+    <p>By creating an account you agree to our <a href="https://www.youtube.com/watch?v=At8v_Yc044Y" style="color:midnightblue">Terms & Privacy</a>.</p>
+
+ 
+    <div class="clearfix">
+      <button type="button" class="cancelbtn">Cancel</button>
+      <button type="submit" class="signupbtn">Sign Up</button>
+    </div>
+  </div>
+</form>
